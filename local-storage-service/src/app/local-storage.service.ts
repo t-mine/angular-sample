48663d352ae81e0ff1c7696aa856c8e69cb6eb10
t-mine <tomonori.mine.biz@gmail.com>
2021-11-01 15:17:19 +0900

    ローカルストレージへのCRUDをリアクティブにする処理追加

--- local-storage-service/src/app/local-storage.service.ts
+++ local-storage-service/src/app/local-storage.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 const MY_STORAGE_KEY = 'my_storage_key';
 
@@ -12,30 +13,57 @@ export interface IMyItem {
   providedIn: 'root'
 })
 export class LocalStorageService {
-  constructor() { }
+
+  public items: IMyItem[] | undefined;
+  public subject = new Subject<IMyItem[]>();
+
+  constructor() {
+  }
 
   // データの取り出し
-  fetch(): IMyItem[] {
+  _fetch(): IMyItem[] {
     return JSON.parse(localStorage.getItem(MY_STORAGE_KEY)?? '[]');
+  }
+  fetch(): Subject<IMyItem[]> {
+    this.items = this._fetch();
+
+    this.subject.next(this.items);
+    console.log('fetch');
+    return this.subject;
   }
 
   // 全削除
-  clear(): void {
-      localStorage.removeItem(MY_STORAGE_KEY);
+  clear(): LocalStorageService {
+    localStorage.removeItem(MY_STORAGE_KEY);
+
+    this.items = this._fetch();
+    this.subject.next(this.items);
+    return this;
   }
 
   // 保存
-  add(myItem: IMyItem): void {
-      const items = this.fetch().concat(myItem);
-      localStorage.setItem(MY_STORAGE_KEY, JSON.stringify(items));
+  add(myItem: IMyItem): LocalStorageService {
+    const filteredItems = this._fetch().filter((_item) => {
+      return _item.id !== myItem.id;
+    });
+    const items = filteredItems.concat(myItem);
+    localStorage.setItem(MY_STORAGE_KEY, JSON.stringify(items));
+
+    this.items = this._fetch();
+    this.subject.next(this.items);
+    return this;
   }
 
   // 1件削除
-  delete(myItem: IMyItem): void {
-      const items = this.fetch();
-      const filteredItems = items.filter((_item) => {
-          return _item.id !== myItem.id;
-      });
-      localStorage.setItem(MY_STORAGE_KEY, JSON.stringify(filteredItems));
+  delete(myItem: IMyItem): LocalStorageService {
+    const items = this._fetch();
+    const filteredItems = items.filter((_item) => {
+      return _item.id !== myItem.id;
+    });
+    localStorage.setItem(MY_STORAGE_KEY, JSON.stringify(filteredItems));
+    
+    this.items = this._fetch();
+    this.subject.next(this.items);
+    return this;
   }
 }
--- local-storage-service/src/app/user/user.component.ts
+++ local-storage-service/src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalStorageService, IMyItem } from '../local-storage.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -8,14 +9,49 @@ import { LocalStorageService, IMyItem } from '../local-storage.service';
 })
 export class UserComponent implements OnInit {
 
-  private items: IMyItem[] | undefined;
+  items: IMyItem[] | undefined;
+  dataSubs = new Subscription();
 
   constructor(
     private localStorageService: LocalStorageService
   ) { }
 
   ngOnInit(): void {
-    this.items = this.localStorageService.fetch();
+
+    this.localStorageService.subject.subscribe((items: IMyItem[]) => {
+      console.log(items);
+      this.items = items;
+    });
+
+    const item1: IMyItem = {
+      id: 1,
+      name: '田中太郎',
+      companyName: 'A社'
+    };
+    const item2: IMyItem = {
+      id: 2,
+      name: '佐藤太郎',
+      companyName: 'C社'
+    };
+    this.localStorageService.add(item1).add(item2);
+
+    const item3: IMyItem = {
+      id: 3,
+      name: '佐藤太郎',
+      companyName: 'F社'
+    };
+
+    this.localStorageService.add(item3);
+  }
+
+  clear(): void {
+    this.localStorageService.clear();
+  }
+
+  ngOnDestroy() {
+    if (this.dataSubs) {
+      this.dataSubs.unsubscribe();
+    }
   }
 
 }
